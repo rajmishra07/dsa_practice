5519b5a8800b1d8e46f96e02bc8b6e9946cca36a
rajmishra07 <mrrajmishra573@gmail.com>
2023-08-06 08:24:14 +0530

    first time adding dsa file

---
+++ DSA_practice.cpp
@@ -0,0 +1,563 @@
+   ______________________#Patterns in DSA 
+
+	1111
+	2222
+	3333
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<("Enter the vlue of n = ");
+    cin>>n;
+    int i=1;   
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i;
+            j++;
+        }   
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+2) 1
+   22
+   333
+   4444
+
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter the value of n= ";
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        while (j<=i){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+3)
+  1
+  12
+  123
+  1234
+  #include <iostream>
+
+*******************************************************************************/
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter the value of n= ";
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        while (j<=i){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+4)
+  1
+  23
+  456
+  78910
+
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter the value of n= ";
+    cin>>n;
+    int k=1;
+    while(i<=n){
+        int j=1;
+        while (j<=i){
+            cout<<k;
+            k++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+5)
+  1
+  23
+  345
+  4567
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter the value of n= ";
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        int k=i;
+        while (j<=i){
+            cout<<k;
+            k++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+6)
+  1
+  21
+  321
+  4321
+
+
+
+#One more logic is added
+
+*@Note   cout<<(row-col+1);          
+
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter the value of n= ";
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        int k=i;
+        while (j<=i){
+            cout<<k;
+            k--;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+
+7)
+A A A
+B B B
+C C C
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row=1;
+    int n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            if(row==1){
+                cout<<"A";
+            }
+            else if(row==2){
+                cout<<"B";
+            }
+            else if(row==3){
+                cout<<"C";
+            }
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+
+    return 0;
+}
+
+
+#one more logic is added
+cout<<('A'+row-1);
+
+Because ASCI value of A + 1 = B
+Because ASCI value of A + 1 = C
+
+
+8)
+   ABC
+   ABC
+   ABC
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row=1;
+    int n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            if(col==1){
+                cout<<"A";
+            }
+            else if(col==2){
+                cout<<"B";
+            }
+            else if(col==3){
+                cout<<"C";
+            }
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+
+    return 0;
+}
+
+
+# one more logic is added
+cout<<ch=('A'+col - 1);
+Because ASCI value of A + 1 = B
+Because ASCI value of A + 1 = C
+
+
+9)
+
+ABC
+DEF
+GHI
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row=1;
+    int n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    char ch = 'A';
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            cout<<ch;
+            ch++;
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+
+    return 0;
+}
+
+10)
+A
+BB
+CCC
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+         
+        while(j<=i){
+            char ch = 'A'+i-1;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+11)
+   A
+   BC
+   CDE
+   EFGH
+
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        char ch = 'A'+i-1;
+        while(j<=i){
+            cout<<ch;
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+12)
+   A
+   BC
+   DEF
+   GHI
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    char ch = 'A';
+    while(i<=n){
+        j=1;
+        while(j<=i){
+            cout<<ch;
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+13)
+   ABC
+   CDE
+   EFG
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        char ch = 'A'+i-1;
+        while(j<=n){
+            cout<<ch;
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+14)*
+   **
+   ***
+   ****
+
+
+   *******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    int n;
+    cout<<"Enter the value of n =";
+    cin>>n;
+     while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+15)
+       *
+     * *
+   * * *
+ * * * *
+
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the vlue of n = ";
+    cin>>n;
+    while(i<=n){
+        int space = n-i;
+        while(space!=0){
+            cout<<" ";
+            space--;
+        }
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+}
+
+
+16)
+
+* * * *
+* * *
+* *
+*
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=0,n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+    i = n-i;
+    while(i!=0){
+        int j=1;
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i--;
+    }
+    return 0;
+}
+
+
+17)
+
+   * * * *
+     * * * 
+       * * 
+         *
+
+
+         
+
+
+
+
+
+18)
+
+   *
+  * *
+ * * *
+* * * * 
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,j,n;
+    cout<<"Enter the vlue of n = ";
+    cin>>n;
+    while(i<=n){
+        int space = n-i;
+        while(space!=0){
+            cout<<" ";
+            space--;
+        }
+        int j=1;
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+}
+
+
+
+
+
+
+
+
